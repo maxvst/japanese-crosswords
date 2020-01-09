be2f5bb96a841d7437b50f4d01ff0f9b0d303001
Максим <max.v@mail.ru>
2020-01-09 09:51:46 +0300

    style update

--- crossword-app/src/app/components/cell/cell.component.ts
+++ crossword-app/src/app/components/cell/cell.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { UnraveledCell, CommonCellStatus } from 'src/app/models/field-status.model';
 
 @Component({
@@ -15,6 +15,11 @@ export class CellComponent implements OnInit {
   unknown = CommonCellStatus.Unknown;
 
   constructor() { }
+
+  @HostBinding('class.cell') get isCell() { return true; }
+  @HostBinding('class.cell_black') get isBlack() { return this.value === this.black; }
+  @HostBinding('class.cell_white') get isWhite() { return this.value === this.white; }
+  @HostBinding('class.cell_unknown') get isUnknown() { return this.value === this.unknown; }
 
   ngOnInit() {
   }
